20ea11b684155a789bf2e443435ef9bbc75db571
Vinicius_Muliterno <viniciusmuliternoti@hotmail.com>
2024-12-06 00:44:44 -0300

    Creating initial project
    project start commit basic structure.

---
+++ src/main/java/com/vtsr/ConsumingSpotifyApi/ConsumingSpotifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.vtsr.ConsumingSpotifyApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingSpotifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingSpotifyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vtsr/ConsumingSpotifyApi/ConsumingSpotifyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtsr.ConsumingSpotifyApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingSpotifyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
